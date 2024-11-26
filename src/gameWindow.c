97c4f3ddf30f2e1c701c2194b8e634faa31ce5d0
Gault115 <gaultierpeyrard2003@gmail.com>
2024-11-26 18:25:39 +0100

    ajustage des fps plus affichage du temps passé en game (il y un petit probleme à corriger par rapport à cela)

--- src/gameWindow.c
+++ src/gameWindow.c
@@ -17,8 +17,32 @@
 #define LONGUEUR_P 22
 
 /*
-R: créer la fenetre de jeu
-E: taille x et y de la fenetre
+R: affiche le temps 
+E: temps en hh:mm:ss en tableau de char
+S: rien
+*/
+
+void printTime(char* t){
+    int i;
+    int h = MLV_get_desktop_height();  /*recupere la taille de l'ecran*/
+    int w = MLV_get_desktop_width();
+    MLV_Font* font;
+    i=MLV_path_exists("../fich/04B_30__.TTF");
+    MLV_draw_filled_rectangle((w/20)*2, h/3+20,(w/20)*2,25,MLV_COLOR_BLACK);
+    if (i==1){
+        font = MLV_load_font( "../fich/04B_30__.TTF" , 20 );
+        MLV_draw_text_with_font(
+            (w/20)*2, h/3+25,
+            t,         /*affiche le temps aux bonnes coordonnées*/
+            font, MLV_COLOR_WHITE
+        );
+    }
+
+}
+
+/*
+R: créer la fenêtre de jeu
+E: taille de l'ecran
 S: rien
 */
 
@@ -38,6 +62,11 @@ void createGameWindow(int w,int h){
     MLV_draw_text_with_font(
         (w/20)*13+25, h/20,
         "NEXT",         /*affiche NEXT aux bonnes coordonnées*/
+        font, MLV_COLOR_WHITE
+    );
+    MLV_draw_text_with_font(
+        (w/20)*2, h/3,
+        "TIME",         /*affiche TIME aux bonnes coordonnées*/
         font, MLV_COLOR_WHITE
     );
     MLV_draw_text_with_font(
@@ -83,6 +112,12 @@ void stopWindow(int w, int h){
     );
     MLV_actualise_window();
 }
+
+/*
+R: créer le menu pause
+E: taille de l'écran et le plateau
+S: rien
+*/
 
 int setStopWindow(int w,int h,plateau* p){
     int x,y,i=1,j=1; /*initialise i et j à vrai*/
@@ -140,7 +175,7 @@ int setStopWindow(int w,int h,plateau* p){
                 }
                 if (x>(w/5+(w/25)*9) && x<(w/5+(w/25)*12) && y>(h/2-h/10+260) && y<(h/2-h/10+50+310)){
                         /*si on clique sur no, reviens au menu pause*/
-                    j=0;/*j passe à fauxs*/
+                    j=0;/*j passe à faux*/
                 }
             }
             
@@ -158,7 +193,12 @@ S: rien
 */
 
 void setGameWindow(plateau *p){
-    int i,width,height,j,k,t,w,h;
+    int i,width,height,j,k,t,w,h,hours,minutes,seconds;
+    struct timespec debut, fin;
+    char text[32];
+    int frame =0;
+    uint temps=0; /*uint pour unsigned int*/
+    temps=0;
     i=1;
     srand(time(NULL));
     height = MLV_get_desktop_height();  /*recupere la taille de l'ecran*/
@@ -170,6 +210,7 @@ void setGameWindow(plateau *p){
     MLV_actualise_window();
     while(i){
         MLV_actualise_window();
+        clock_gettime(CLOCK_REALTIME, &debut);
         if (MLV_get_keyboard_state(MLV_KEYBOARD_ESCAPE)== MLV_PRESSED){
             /*si on appuie sur espace, alors on ouvre le menu pause*/
             i=setStopWindow(width,height,p);
@@ -187,8 +228,18 @@ void setGameWindow(plateau *p){
                 MLV_draw_filled_rectangle((w/20)*13+(t+1)*k,(h/20)*2+(t+1)*j,t,t,MLV_rgba(rand()%256,rand()%256,rand()%256,255));
                 /*test grille prochaine pièce*/
             }
-        }   
-        MLV_wait_milliseconds(33);
+        }
+        if (frame%30==0){
+            temps ++;
+            hours = temps / 3600;
+            minutes = (temps % 3600) / 60;
+            seconds = temps % 60;
+            sprintf(text, "%02d:%02d:%02d", hours, minutes, seconds); /* Met un texte avec des int dans une variable "string"*/
+            printTime(text);
+        }
+        frame=(frame + 1)%30;
+        clock_gettime(CLOCK_REALTIME, &fin);
+        if((1000/30)-(((fin.tv_sec*1000)+(fin.tv_nsec/1000000))-((debut.tv_sec*1000)+(debut.tv_nsec/1000000)))>0) MLV_wait_milliseconds((1000/30)-(((fin.tv_sec*1000)+(fin.tv_nsec/1000000))-((debut.tv_sec*1000)+(debut.tv_nsec/1000000))));
     }
 }
 
