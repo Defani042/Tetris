38e711a7e3be6deea6fd9dd2282fcfd017334fe4
Gault115 <gaultierpeyrard2003@gmail.com>
2024-11-29 12:46:00 +0100

    reglage de cons flis

--- src/main.c
+++ src/main.c
@@ -16,21 +16,14 @@ int main(){
 
   plateau p;
   plateau* pp = &p;
-  piece p1;
-  piece* pp1 = &p1;
-  
+ 
 
   /*Srand*/
   srand(time(NULL));
   
 /*Put the main code here*/
   setPlateau(pp);/*init le plateau*/
-<<<<<<< HEAD
-  printTabPiece(pp->tpiece);
 
-=======
-  
->>>>>>> 68a50e3848acb0fc2172a14e273a4685d9e70fb1
 /*Test MainWindows*/  
   SetMainWindow(pp);
   exit(EXIT_SUCCESS);
